38071f9a7735f3bc7cc28096a991ae038c882e1a
Nikhil Jagtap <nikhilj1711@gmail.com>
2025-12-02 20:41:19 +0530

    first-commit

---
+++ src/main/java/com/edigest/journalApp/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.edigest.journalApp.repository;
+
+import com.edigest.journalApp.entity.JournalEntry;
+import com.edigest.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUserName(String username);
+
+    void deleteByUserName(String name);
+}
+
---
+++ src/test/java/com/edigest/journalApp/service/UserDetailsServiceImplTests.java
@@ -0,0 +1,42 @@
+package com.edigest.journalApp.service;
+
+import com.edigest.journalApp.entity.User;
+import com.edigest.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+
+//@ActiveProfiles("dev")
+public class UserDetailsServiceImplTests {
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void loadUserByUserNameTest(){
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("ichdifni").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailsService.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+    }
+}
